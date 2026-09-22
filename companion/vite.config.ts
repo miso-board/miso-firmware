3db6f262d887a45731fa88724a6a0a7bdfa98cb8
Hex <hex@misoboard.io>
2026-09-22 16:14:58 +0100

    move companion app to separate repo

--- companion/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import tailwindcss from "@tailwindcss/vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
-
-export default defineConfig({
-  plugins: [svelte(), tailwindcss(), viteSingleFile()],
-});
